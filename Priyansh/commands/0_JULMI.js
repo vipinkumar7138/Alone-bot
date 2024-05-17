04e31f7e005c242bf7dc084407d80b0e423a1a97
Nvnnain <mr3175647@gmail.com>
2024-05-17 13:35:01 +0530

    Update 0_JULMI.js

--- Priyansh/commands/0_JULMI.js
+++ Priyansh/commands/0_JULMI.js
@@ -19,7 +19,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
      react.includes("Julmi") || 
 react.includes("JULMI")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦🌸===『*★🌸◉❖जुल्मी≛जाट❖◉✦\n\n★★᭄𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐀𝐁𝐔 𝐊𝐀 :\n\n✦ https://instagram.com/_julmi_jaat__?igshid=OGQ5ZDc2ODk2ZA==  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐀𝐁𝐔 𝐊𝐀 😁\n 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 𝐋𝐔𝐊 😁😋 https://www.facebook.com/julmijaat1008?mibextid=ZbWKwL`",
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦🌸===『*★🌸◉❖जुल्मी≛जाट❖◉✦\n\n★★᭄𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 :\n\n✦ https://instagram.com/_julmi_jaat__?igshid=OGQ5ZDc2ODk2ZA==  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀😁😋 https://www.facebook.com/julmijaat1008?mibextid=ZbWKwL`",
 				attachment: fs.createReadStream(__dirname + `/noprefix/Julmi.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
