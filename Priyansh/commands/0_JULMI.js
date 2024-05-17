c659a933235289c40ff4068a6dfb8feb8ae73c2e
Nvnnain <mr3175647@gmail.com>
2024-05-17 13:44:32 +0530

    Update 0_JULMI.js

--- Priyansh/commands/0_JULMI.js
+++ Priyansh/commands/0_JULMI.js
@@ -14,10 +14,11 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
 	if(react.includes("जुल्मी जाट") ||
-     react.includes("julmi") || 
-     react.includes("जुल्मी") || 
-     react.includes("Julmi") || 
-react.includes("JULMI")) {
+     react.includes("Owner") || 
+     react.includes("owner") || 
+     react.includes("ADMIN") ||
+     react.includes("admin") || 
+react.includes("Admin")) {
 		var msg = {
 				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦🌸===『*★🌸◉❖जुल्मी≛जाट❖◉✦\n\n★★᭄𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 :\n\n✦ https://instagram.com/_julmi_jaat__?igshid=OGQ5ZDc2ODk2ZA==  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀😁😋 https://www.facebook.com/julmijaat1008?mibextid=ZbWKwL`",
 				attachment: fs.createReadStream(__dirname + `/noprefix/Julmi.jpg`)
