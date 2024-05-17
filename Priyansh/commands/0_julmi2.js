ac0b24b65be892b0b93513846d4f49c28b5f53a5
Nvnnain <mr3175647@gmail.com>
2024-05-17 10:34:50 +0530

    Add files via upload

---
+++ Priyansh/commands/0_julmi2.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "Funny bot",
+    version: "1.1.1",
+  hasPermssion: 0,
+  credits: "𝑱𝑼𝑳𝑴𝑰 𝑱𝑨𝑨𝑻", 
+  description: "Just Respond",
+  commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  let react = event.body.toLowerCase();
+  if(react.includes("Kutta bot") ||
+     react.includes("bot kutta") || react.includes("KUTTA BOT") || react.includes("kutta bot") ||
+react.includes("bakk") ||
+react.includes("Bakk bot")) {
+    var msg = {
+        body: `【 𝐁𝐀𝐂𝐇 𝐊𝐄 𝐑𝐀𝐇𝐄𝐍𝐀 𝐊𝐀𝐇𝐈 𝐌𝐄𝐊O D𝐈𝐋 𝐌𝐀𝐓 𝐃𝐄𝐊𝐄 𝐁𝐀𝐈𝐓𝐇 𝐉𝐀𝐍𝐀🙂🖐️ 】`,
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😙", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
--- Priyansh/commands/status.js
+++ Priyansh/commands/status.js
@@ -2,36 +2,26 @@ module.exports.config = {
   name: "status",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-  description: "log",
-  commandCategory: "System",
-  usages: "",
-  cooldowns: 3,
-  denpendencies: {
+  credits: "Modded By Arun",
+  description: "RANDOM Status video",
+  commandCategory: "Random video",
+  usages: "status",
+  cooldowns: 2,
+  dependencies: {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
   }
+    
 };
 
-module.exports.run = async function ({ api, event, Threads, getText }) {
-  const fs = global.nodemodule["fs-extra"];
-  var { threadID, messageID, senderID } = event;
-  //if (senderID == global.data.botID) return;
-
-  var dataThread = (await Threads.getData(threadID));
-  var data = dataThread.data;
-  //console.log(data)
-  //var prefix = data.PREFIX;
-  var rankup = data.rankup;
-  var resend = data.resend;
-  var log = data.log;
-  var tagadmin = data.tagadmin;
-  var guard = data.guard;
-  var antiout = data.antiout;
-  //prefix == null ? rankup = `!` : rankup = `${prefix}`;
-  log == null ? log = `true` : log = `${log}`;
-  rankup == null ? rankup = `false` : rankup = `${rankup}`;
-  resend == null ? resend = `false` : resend = `${resend}`;
-  tagadmin == null ? tagadmin = `true` : tagadmin = `${tagadmin}`;
-  guard == null ? guard = `true` : guard = `${guard}`;
-  antiout == null ? antiout = `true` : antiout = `${antiout}`;
-return api.sendMessage(`ᅠᅠ☣️Table ☣️ \n\n\n🍄────•🦋• ────🍄\n❯ 🍉 Log: ${log}\n❯ 🍇 Rankup: ${rankup}\n❯ 🍓 Resend: ${resend}\n❯ 🥕 Tag admin: ${tagadmin}\n❯ 🍑 Antirobbery ${guard}\n❯ 🍒 Antiout: ${antiout}\n🍄────•🦋• ────🍄`, threadID, messageID);
-}
+module.exports.run = async({api,event,args,Users,Threads,Currencies}) => {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+    var link = [
+"https://i.imgur.com/jIFYoox.mp4" , "https://i.imgur.com/w971tct.mp4" , "https://i.imgur.com/JNTP6OI.mp4" , "https://i.imgur.com/AY4eWDB.mp4" , "https://i.imgur.com/SnPt790.mp4" , "https://i.imgur.com/iQjJ5av.mp4" , "https://i.imgur.com/J6rJPXW.mp4"
+     ];
+     var callback = () => api.sendMessage({body:`💝 𝗛𝗼𝗽𝗲 𝘆𝗼𝘂 𝗟𝗶𝗸𝗲 𝗜𝘁, \n♥️    𝗠𝗮𝗱𝗲 𝗕𝘆 𝗔𝗿𝘂𝗻🫥`,attachment: fs.createReadStream(__dirname + "/cache/1.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.mp4"));
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.mp4")).on("close",() => callback());
+   };
