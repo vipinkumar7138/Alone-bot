71b9a91a45607b8a1163ef859ac2d0d624a98296
Julmi1008 <148467720+Julmi1008@users.noreply.github.com>
2024-05-23 13:07:23 +0530

    Update 0गोलगप्पा.js

--- Priyansh/commands/0गोलगप्पा.js
+++ Priyansh/commands/0गोलगप्पा.js
@@ -22,6 +22,6 @@ const fs = global.nodemodule["fs-extra"];
     var link = [
 "https://i.imgur.com/wkgZclp.jpg",
      ];
-     var callback = () => api.sendMessage({body:`❤️💝𝐌𝐚𝐝𝐞 𝐁𝐲:-☞𝗝𝗨𝗟𝗠𝗜 𝗝𝗔𝗔𝗧☜\nये लो बाबू गोलगप्पा खाओ😘 -.-`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
+     var callback = () => api.sendMessage({body:`❤️💝𝐌𝐚𝐝𝐞 𝐁𝐲:-☞𝗝𝗨𝗟𝗠𝗜 𝗝𝗔𝗔𝗧☜\nये लो बाबू गोलगप्पा खाओ😘 -.-`,attachment: fs.createReadStream(__dirname + "/cache/2.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/2.jpg"));  
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/2.jpg")).on("close",() => callback());
    }
