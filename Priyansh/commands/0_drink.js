7f272272a5f5216ae9a3208b0ceebc5f5e82bccc
Julmi1008 <148467720+Julmi1008@users.noreply.github.com>
2024-05-23 10:55:29 +0530

    Update 0_drink.js

--- Priyansh/commands/0_drink.js
+++ Priyansh/commands/0_drink.js
@@ -12,7 +12,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
-  if (event.body.indexOf("drink")==0 || event.body.indexOf("DRINK")==0 || event.body.indexOf("DARU")==0 || event.body.indexOf("daru")==0) {
+  if (event.body.indexOf("Drink")==0 || event.body.indexOf("drink")==0 || event.body.indexOf("DRINK")==0 || event.body.indexOf("daru")==0) {
     var msg = {
         body: "─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─\n आ जाओ मिलकर पार्टी करे🍺",
         attachment: fs.createReadStream(__dirname + `/cache/drink.jpg`)
