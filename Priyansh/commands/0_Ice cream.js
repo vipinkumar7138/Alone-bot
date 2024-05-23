acf2adc177286f9f6a87b3bb0ee120ec092c75ee
Julmi1008 <148467720+Julmi1008@users.noreply.github.com>
2024-05-23 07:26:24 +0530

    Update 0_Ice cream.js

--- Priyansh/commands/0_Ice cream.js
+++ Priyansh/commands/0_Ice cream.js
@@ -1,28 +1,26 @@
+const fs = require("fs");
 module.exports.config = {
   name: "icecream",
-  version: "1.0.0",
+    version: "1.0.1",
   hasPermssion: 0,
-  credits: "BLACK",
-  description: "Random 6-pack photos",
-  commandCategory: "Random-IMG",
-  usages: "icecream ",
-  cooldowns: 2,
-  dependencies: {
-    "request":"",
-    "fs-extra":"",
-    "axios":""
-  }
-
+  credits: "VanHung - Fixed by LTD", 
+  description: "hihihihi",
+  commandCategory: "no prefix",
+  usages: "icecream",
+    cooldowns: 5, 
 };
 
-module.exports.run = async({api,event,args,Users,Threads,Currencies}) => {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
-    var link = [
-"https://i.imgur.com/cPz1X2x.jpg",
-"https://i.imgur.com/XCLiqfy.jpg",
-     ];
-     var callback = () => api.sendMessage({body:`─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─\n❤️ ये लो बाबू आइस्क्रीम खाओ😋 -.-`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
-   }
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("Icecream")==0 || event.body.indexOf("icecream")==0 || event.body.indexOf("ice-cream")==0 || event.body.indexOf("ICE-CREAM")==0) {
+    var msg = {
+        body: "─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─\nये लो बाबू आइस्क्रीम खाओ😋",
+        attachment: fs.createReadStream(__dirname + `/cache/1.jpg`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🥶", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+                                        }
