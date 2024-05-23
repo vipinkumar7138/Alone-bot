d376dfb986ee8592e1b4d20f39b7a37bcbd1e0c7
Julmi1008 <148467720+Julmi1008@users.noreply.github.com>
2024-05-23 11:42:00 +0530

    Add files via upload

---
+++ Priyansh/commands/0_stng.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "seeting",
+    version: "1.0.1",
+  hasPermssion: 0,
+  credits: "VanHung - Fixed by LTD", 
+  description: "hihihihi",
+  commandCategory: "no prefix",
+  usages: "seeting",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("sting")==0 || event.body.indexOf("Stng")==0 || event.body.indexOf("gf")==0 || event.body.indexOf("maal")==0) {
+    var msg = {
+        body: "─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─\n ले बेटा जब तक सेटिंग ना होती इसको पकड़ ले😜",
+        attachment: fs.createReadStream(__dirname + `/cache/stng.jpg`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😁", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+                                        }
